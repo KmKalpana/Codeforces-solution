c61eaa4209bb2e30af3554372c8830a2d324939c
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-10-29 09:47:42 +0530

    solved 1038-B problem

--- Maths/1038B_non-coprime-partition.cpp
+++ Maths/1038B_non-coprime-partition.cpp
@@ -5,4 +5,27 @@ int main()
 {
     int n;
     cin>>n;
+    if(n<=2)
+     cout<<"No"<<endl;
+     else
+     {
+        cout<<"Yes"<<endl;
+       /* cout<<1<<" "<<n<<endl;
+        cout<<n-1<<" ";
+        for(int i=1; i<=n-1; i++)
+          cout<<i<<" ";
+        cout<<endl;
+        */
+        //Second Method
+        int k=n%2==0?n/2:((n+1)/2);
+        cout<<1<<" "<<k<<endl;
+        cout<<n-1<<" ";
+        for(int i=1; i<=n; i++)
+        {
+            if(i==k)
+             continue;
+             cout<<i<<" ";
+        }
+        cout<<endl;
+     }
 }
