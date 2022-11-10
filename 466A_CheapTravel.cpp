02d8e09c749811963b0cebc0bb26b60036f22038
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-11-10 20:26:18 +0530

    Cheap Travel

---
+++ 466A_CheapTravel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,a,b;
+    cin>>n>>m>>a>>b;
+    if(m*a<=b)
+    {
+        cout<<n*a<<endl;
+    }
+    else
+    {
+        cout<<((n/m)*b+min((n%m)*a,b))<<endl;
+    }
+    return 0;
+}
